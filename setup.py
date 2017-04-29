f596c3c7a63501d4046d73fd8b8d2fe3820000c4
angelocarbone <angelo.carbone@gmail.com>
2017-04-29 11:24:42 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+"""pyiqoapi is a python wrapper for IQOption's API."""
+from setuptools import (setup, find_packages)
+
+setup(
+    name="pyiqoapi",
+    version="0.1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    description="pyiqoapi is a python wrapper for IQOption's API.",
+    long_description="pyiqoapi is a python wrapper for IQOption's API.",
+    url="https://github.com/angelocarbone/pyiqoapi",
+    author="angelocarbone",
+    zip_safe=False
+)
