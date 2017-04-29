ba61fc28a6fd730edd132ab2dc7ab33102a8ecfc
angelocarbone <angelo.carbone@gmail.com>
2017-04-29 12:07:13 +0200

    Update setup.py
    From 0.1.0 to 0.2.0 version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import (setup, find_packages)
 
 setup(
     name="pyiqoapi",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(),
     include_package_data=True,
     description="pyiqoapi is a python wrapper for IQOption's API.",
