33103189b0c8a75697e0c836c53d312f005ef5cd
angelocarbone <angelo.carbone@gmail.com>
2017-04-29 17:29:55 +0200

    Update setup.py
    Improve setup settings

--- setup.py
+++ setup.py
@@ -1,14 +1,36 @@
 """pyiqoapi is a python wrapper for IQOption's API."""
-from setuptools import (setup, find_packages)
+from setuptools import setup, find_packages
 
-setup(
-    name="pyiqoapi",
-    version="0.2.0",
-    packages=find_packages(),
-    include_package_data=True,
-    description="pyiqoapi is a python wrapper for IQOption's API.",
-    long_description="pyiqoapi is a python wrapper for IQOption's API.",
-    url="https://github.com/angelocarbone/pyiqoapi",
-    author="angelocarbone",
-    zip_safe=False
-)
+version = '0.2.0'
+
+setup(name='pyiqoapi',
+      version=version,
+      description="pyiqoapi is a python wrapper for IQOption's API.",
+      long_description="""\
+""",
+      classifiers=[
+          'Programming Language :: Python',
+          'License :: OSI Approved :: MIT License',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Financial and Insurance Industry'
+          'Operating System :: OS Independent',
+          'Development Status :: 4 - Beta',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='IQOption binary option wrapper REST API',
+      author='angelocarbone',
+      author_email='api@magmati.co',
+      url='http://pyiqo.magmati.co/',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      # test_suite="tests",
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'requests',
+          'websocket-client'
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
